5757c885de31bb2451db01e20084f155999563ff
Braden Fisher <Braden.J.Fisher-1@ou.edu>
2023-09-26 15:18:12 -0500

    Add project files.

---
+++ MIS3033_Exam1_BradenFisher/Data/Exam1DB.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace a
+{
+    public class ExamDB:DbContext
+    {// we need to add database tables here DBSet<>
+
+        protected override void OnConfiguring(DbContextOptionsBuilder opt)
+        {
+            opt.UseSqlite("Data Source = exam.db");
+        }
+    }
+}
---
+++ MIS3033_Exam1_BradenFisher/Program.cs
@@ -0,0 +1,6 @@
+﻿// MIS 3033 002
+// September 27, 2023
+// Start at 10:30 AM, due time is 11:45 AM, closes at 11:50 AM
+// Braden Fisher, 113492081
+
+Console.WriteLine("MIS 3033 Exam 1");
